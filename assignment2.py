f3a714777027559b9b7e9b9168f9d921b8bc4398
Shivangi <saraswatshivangi.21@outlook.com>
2018-05-30 14:38:15 +0530

    My second git commit

---
+++ assignment2.py
@@ -0,0 +1,15 @@
+#Assignment 2
+print ("This is assignment 2")
+print("Acad"+"View")
+x=input("Enter value for x")
+print(x)
+y=input("Enter value for y")
+print(y)
+z=input("Enter value for z")
+print(z)
+print("Let's get started")
+s="Acadview"
+course="Python"
+fees=500
+print('%s %s %d' % (s, course, fees))
+#Ans 6. Tony Stark, 1000000
